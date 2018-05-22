597aaa58f87a6aee0873ddb5c37e123fa2b181cb
Rafael Cardoso <comerciorafael@hotmail.com>
2018-05-22 00:30:46 -0300

    Primeiro projeto WEB API

---
+++ 5_WebAPI/PrimeiraWebAPI/PrimeiraWebAPI/Controllers/TesteController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PrimeiraWebAPI.Controllers
+{
+    [Route("rafael/[controller]")]
+    public class TesteController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Teste 1", "Teste 2", "Teste 3" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Teste {id}";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
