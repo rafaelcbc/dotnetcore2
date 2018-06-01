58132e912871d6d2b72d9015239a8e5cd4c99515
Rafael Cardoso <comerciorafael@hotmail.com>
2018-05-31 23:59:49 -0300

    Projeto ProdutosWebAPI do curso de DotNet Core 2.0

---
+++ 5_WebAPI/ProdutosWebAPI/ProdutosWebAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProdutosWebAPI.Models;
+using System.Collections.Generic;
+
+namespace ProdutosWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProdutosController : Controller
+    {
+        static readonly IProdutoRepositorio repositorio = new ProdutoRepositorio();
+
+        [HttpGet]
+        public IEnumerable<Produto> GetTodos()
+        {
+            return repositorio.GetAll();
+        }
+
+        [HttpGet("{id}" , Name = "GetProduto")]
+        public IActionResult GetProdutoPorId(int id)
+        {
+            Produto produto = repositorio.Get(id);
+            if(produto == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(produto);
+        }
+
+        [HttpPost]
+        public IActionResult CriarProduto([FromBody] Produto item)
+        {
+            if(item == null)
+            {
+                return BadRequest();
+            }
+
+            item = repositorio.Add(item);
+            return CreatedAtRoute("GetProduto", new { id = item.Id }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaProduto(int id, [FromBody] Produto item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            item.Id = id;
+            if(!repositorio.Update(item))
+            {
+                return NotFound();
+            }
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaProduto(int id)
+        {
+            Produto item = repositorio.Get(id);
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            repositorio.Remove(id);
+            return new NoContentResult();
+        }
+    }
+}
---
+++ 5_WebAPI/ProdutosWebAPI/ProdutosWebAPI/Models/IProdutoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProdutosWebAPI.Models
+{
+    public interface IProdutoRepositorio
+    {
+        IEnumerable<Produto> GetAll();
+        Produto Get(int id);
+        Produto Add(Produto item);
+        bool Update(Produto item);
+        void Remove(int id);
+    }
+}
